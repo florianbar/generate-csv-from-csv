21fb128cd58e843777ed561bbd1b2a908b483bb0
Florian Bär <florianbar01@gmail.com>
2025-07-02 15:03:01 +0200

    add formik and yup to manage form state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { Formik, Form, Field } from "formik";
+import { object, string } from "yup";
+
+interface FormValues {
+  wallet_id: string;
+  csv: File | string;
+  outputName: string;
+}
+
 const WALLET_ADDRESSES = {
   MAIN: { name: "Main", id: "0.0.1452054" },
   SECONDARY: { name: "Secondary", id: "0.0.1874847" },
@@ -7,11 +16,21 @@ const WALLET_ADDRESSES = {
   EDF_STAKING: { name: "EDF Staking", id: "0.0.1977756" },
 };
 
-export default function Home() {
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+const formSchema = object({
+  wallet_id: string().required("Wallet ID is required"),
+  csv: string().required("CSV is required"),
+  outputName: string().required("Output name is required"),
+});
 
-    const formData = new FormData(e.target as HTMLFormElement);
+export default function Home() {
+  const handleSubmit = async (values: FormValues) => {
+    const formData = new FormData();
+    formData.append("wallet_id", values.wallet_id);
+    if (values.csv) {
+      formData.append("csv", values.csv);
+    }
+    formData.append("output-name", values.outputName);
+
     const response = await fetch("/api/csv", {
       method: "POST",
       body: formData,
@@ -33,29 +52,56 @@ export default function Home() {
           <p>Convert a Hedera CSV into a valid Koinly CSV.</p>
         </div>
 
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <label htmlFor="wallet_id">Wallet ID</label>
-          <select name="wallet_id">
-            <option value="" disabled>
-              Select a wallet
-            </option>
-            {Object.entries(WALLET_ADDRESSES).map(([key, value]) => (
-              <option key={key} value={value.id}>
-                {value.name} - {value.id}
-              </option>
-            ))}
-          </select>
+        <Formik
+          initialValues={{ wallet_id: "", csv: "", outputName: "" }}
+          onSubmit={handleSubmit}
+          validationSchema={formSchema}
+        >
+          {({ errors, touched }) => (
+            <Form className="space-y-4">
+              <div>
+                <label htmlFor="wallet_id">Wallet ID</label>
+                <Field as="select" name="wallet_id">
+                  <option value="" disabled>
+                    Select a wallet
+                  </option>
+                  {Object.entries(WALLET_ADDRESSES).map(([key, value]) => (
+                    <option key={key} value={value.id}>
+                      {value.name} - {value.id}
+                    </option>
+                  ))}
+                </Field>
+                <div className="error">
+                  {errors.wallet_id && touched.wallet_id ? (
+                    <div>{errors.wallet_id}</div>
+                  ) : null}
+                </div>
+              </div>
 
-          <label htmlFor="csv">Upload CSV</label>
-          <input type="file" name="csv" />
+              <div>
+                <label htmlFor="csv">Upload CSV</label>
+                <Field as="input" type="file" name="csv" />
+                <div className="error">
+                  {errors.csv && touched.csv ? <div>{errors.csv}</div> : null}
+                </div>
+              </div>
 
-          <label htmlFor="output-name">Output name</label>
-          <input type="text" name="output-name" />
+              <div>
+                <label htmlFor="output-name">Output name</label>
+                <Field as="input" type="text" name="outputName" />
+                <div className="error">
+                  {errors.outputName && touched.outputName ? (
+                    <div>{errors.outputName}</div>
+                  ) : null}
+                </div>
+              </div>
 
-          <div className="flex justify-end">
-            <button type="submit">Generate CSV</button>
-          </div>
-        </form>
+              <div className="flex justify-end">
+                <button type="submit">Generate CSV</button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
