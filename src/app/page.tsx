1fcb818d0af47ca3729ce029fecf579b83376c5b
Florian Bär <florianbar01@gmail.com>
2025-07-02 14:48:57 +0200

    add form fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+const WALLET_ADDRESSES = {
+  MAIN: { name: "Main", id: "0.0.1452054" },
+  SECONDARY: { name: "Secondary", id: "0.0.1874847" },
+  TATA_STAKING: { name: "Tata Staking", id: "0.0.1874888" },
+  EDF_STAKING: { name: "EDF Staking", id: "0.0.1977756" },
+};
+
 export default function Home() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -19,11 +26,37 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="file" name="csv" />
-        <button type="submit">Upload</button>
-      </form>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="max-w-sm w-full">
+        <div className="mb-6">
+          <h1>Hedera CSV Generator</h1>
+          <p>Convert a Hedera CSV into a valid Koinly CSV.</p>
+        </div>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <label htmlFor="wallet_id">Wallet ID</label>
+          <select name="wallet_id">
+            <option value="" disabled>
+              Select a wallet
+            </option>
+            {Object.entries(WALLET_ADDRESSES).map(([key, value]) => (
+              <option key={key} value={value.id}>
+                {value.name} - {value.id}
+              </option>
+            ))}
+          </select>
+
+          <label htmlFor="csv">Upload CSV</label>
+          <input type="file" name="csv" />
+
+          <label htmlFor="output-name">Output name</label>
+          <input type="text" name="output-name" />
+
+          <div className="flex justify-end">
+            <button type="submit">Generate CSV</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
