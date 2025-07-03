a4842e72ff1a9b7b315974f6b42e29bed5605c4d
Florian Bär <florianbar01@gmail.com>
2025-07-03 13:02:14 +0200

    make hedera wallet constant

---
+++ src/constants/hedera.ts
@@ -0,0 +1,6 @@
+export const HEDERA_WALLET_ADDRESSES = {
+  MAIN: { name: "Main", id: "0.0.1452054" },
+  SECONDARY: { name: "Secondary", id: "0.0.1874847" },
+  TATA_STAKING: { name: "Tata Staking", id: "0.0.1874888" },
+  EDF_STAKING: { name: "EDF Staking", id: "0.0.1977756" },
+};
