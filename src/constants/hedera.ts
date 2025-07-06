ff25d7e4bc1de7d97374c29f4a868f9a74ec0177
Florian Bär <florianbar01@gmail.com>
2025-07-06 23:13:32 +0200

    fix wallet id

--- src/constants/hedera.ts
+++ src/constants/hedera.ts
@@ -2,7 +2,7 @@ import { HederaWallet } from "@/types/hedera";
 
 export const WALLETS: Record<string, HederaWallet> = {
   "0.0.1452054": { name: "Main", id: "0.0.1452054" },
-  "0.0.1874847": { name: "Secondary", id: "0.0.1874847" },
+  "0.0.1874846": { name: "Secondary", id: "0.0.1874846" },
   "0.0.1874888": { name: "Tata Staking", id: "0.0.1874888", staking: true },
   "0.0.1977756": { name: "EDF Staking", id: "0.0.1977756", staking: true },
 };
